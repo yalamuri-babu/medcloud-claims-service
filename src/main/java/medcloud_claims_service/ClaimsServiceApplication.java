2b165c1f96ce0c7d2363ebef811b0f97177ae2a8
yalamuri-babu <yalj.gana@gmail.com>
2026-08-17 09:52:19 +0530

    feat: add initial claims service

---
+++ src/main/java/medcloud_claims_service/ClaimsServiceApplication.java
@@ -0,0 +1,13 @@
+package medcloud_claims_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/medcloud_claims_service/ClaimsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package medcloud_claims_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
