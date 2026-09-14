1cf2afabd916cd67311dbe2c24ed0ae9380355e7
yalamuri-babu <yalj.gana@gmail.com>
2026-09-14 10:22:32 +0530

    infra: refactor VPC resources into Terraform module

---
+++ infra/terraform/environments/dev/moved.tf
@@ -0,0 +1,44 @@
+moved {
+  from = aws_vpc.medcloud_dev
+  to   = module.vpc.aws_vpc.medcloud_dev
+}
+
+moved {
+  from = aws_subnet.public_a
+  to   = module.vpc.aws_subnet.public_a
+}
+
+moved {
+  from = aws_subnet.public_b
+  to   = module.vpc.aws_subnet.public_b
+}
+
+moved {
+  from = aws_subnet.private_a
+  to   = module.vpc.aws_subnet.private_a
+}
+
+moved {
+  from = aws_subnet.private_b
+  to   = module.vpc.aws_subnet.private_b
+}
+
+moved {
+  from = aws_internet_gateway.medcloud_dev
+  to   = module.vpc.aws_internet_gateway.medcloud_dev
+}
+
+moved {
+  from = aws_route_table.public
+  to   = module.vpc.aws_route_table.public
+}
+
+moved {
+  from = aws_route_table_association.public_a
+  to   = module.vpc.aws_route_table_association.public_a
+}
+
+moved {
+  from = aws_route_table_association.public_b
+  to   = module.vpc.aws_route_table_association.public_b
+}
--- infra/terraform/environments/dev/outputs.tf
+++ infra/terraform/environments/dev/outputs.tf
@@ -1,20 +1,14 @@
 output "vpc_id" {
   description = "MedCloud DEV VPC ID"
-  value       = aws_vpc.medcloud_dev.id
+  value       = module.vpc.vpc_id
 }
 
 output "public_subnet_ids" {
   description = "Public subnet IDs"
-  value = [
-    aws_subnet.public_a.id,
-    aws_subnet.public_b.id
-  ]
+  value       = module.vpc.public_subnet_ids
 }
 
 output "private_subnet_ids" {
   description = "Private subnet IDs"
-  value = [
-    aws_subnet.private_a.id,
-    aws_subnet.private_b.id
-  ]
+  value       = module.vpc.private_subnet_ids
 }
